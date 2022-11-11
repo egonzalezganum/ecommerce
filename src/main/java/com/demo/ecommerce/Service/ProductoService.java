507c44699e917fd5b29dabf54ef024d94029d564
egonzalezganum <egonzalezganum@gmail.com>
2022-11-11 00:19:21 -0300

    creacion de repository, clases y servicio de producto

---
+++ src/main/java/com/demo/ecommerce/Service/ProductoService.java
@@ -0,0 +1,16 @@
+package com.demo.ecommerce.Service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.demo.ecommerce.model.Producto;
+
+public interface ProductoService {
+
+	public Producto save(Producto producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete (Integer id);
+	
+}
