8a3a7fec926e2aa7fdad04bdb0e32aeb4dd6ac24
egonzalezganum <egonzalezganum@gmail.com>
2022-11-10 23:57:09 -0300

    creacion de clases model, mapeo de clases a tablas con JPA, integrado de diseño y vistas

--- src/main/java/com/demo/ecommerce/model/DetalleOrden.java
+++ src/main/java/com/demo/ecommerce/model/DetalleOrden.java
@@ -1,68 +1,107 @@
 package com.demo.ecommerce.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "detalles")//con este nombre se crea la tabla
 public class DetalleOrden {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private double cantidad;
+	private double precio;
+	private double total;
+	
+	@OneToOne
+	private Orden orden;
 
-    private Integer id;
-    private String nombre;
-    private double cantidad;
-    private double precio;
-    private double total;
+	@ManyToOne
+	private Producto producto;
+	
+	public DetalleOrden() {
+	}
+	
+	public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total, Orden orden,
+			Producto producto) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		this.precio = precio;
+		this.total = total;
+		this.orden = orden;
+		this.producto = producto;
+	}
 
-    public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total) {
-        this.id = id;
-        this.nombre = nombre;
-        this.cantidad = cantidad;
-        this.precio = precio;
-        this.total = total;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public DetalleOrden() {
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public double getCantidad() {
+		return cantidad;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
 
-    public double getCantidad() {
-        return cantidad;
-    }
+	public double getPrecio() {
+		return precio;
+	}
 
-    public void setCantidad(double cantidad) {
-        this.cantidad = cantidad;
-    }
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
 
-    public double getPrecio() {
-        return precio;
-    }
+	public double getTotal() {
+		return total;
+	}
 
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
+	public void setTotal(double total) {
+		this.total = total;
+	}
 
-    public double getTotal() {
-        return total;
-    }
+	
+	
+	public Orden getOrden() {
+		return orden;
+	}
 
-    public void setTotal(double total) {
-        this.total = total;
-    }
+	public void setOrden(Orden orden) {
+		this.orden = orden;
+	}
 
-    @Override
-    public String toString() {
-        return "DetalleOrden [id=" + id + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio
-                + ", total=" + total + "]";
-    }
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	@Override
+	public String toString() {
+		return "DetalleOrden [id=" + id + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio
+				+ ", total=" + total + "]";
+	}
 
 }
--- src/main/java/com/demo/ecommerce/model/Usuario.java
+++ src/main/java/com/demo/ecommerce/model/Usuario.java
@@ -1,100 +1,138 @@
 package com.demo.ecommerce.model;
 
+import java.util.List;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")//con este nombre se crea la tabla
 public class Usuario {
 
-    private Integer id;
-    private String nombre;
-    private String user;
-    private String mail;
-    private String direccion;
-    private String telefono;
-    private String tipo;
-    private String password;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private String user;
+	private String mail;
+	private String direccion;
+	private String telefono;
+	private String tipo;
+	private String password;
 
-    public Usuario() {
-    }
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto> productos;
 
-    public Usuario(Integer id, String nombre, String user, String mail, String direccion, String telefono, String tipo,
-            String password) {
-        super();
-        this.id = id;
-        this.nombre = nombre;
-        this.user = user;
-        this.mail = mail;
-        this.direccion = direccion;
-        this.telefono = telefono;
-        this.tipo = tipo;
-        this.password = password;
-    }
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden> ordenes;
 
-    public Integer getId() {
-        return id;
-    }
+	public Usuario() {
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public Usuario(Integer id, String nombre, String user, String mail, String direccion, String telefono, String tipo,
+			String password, List<Producto> productos, List<Orden> ordenes) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.user = user;
+		this.mail = mail;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.tipo = tipo;
+		this.password = password;
+		this.productos = productos;
+		this.ordenes = ordenes;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getUser() {
-        return user;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public void setUser(String user) {
-        this.user = user;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public String getMail() {
-        return mail;
-    }
+	public String getUser() {
+		return user;
+	}
 
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
+	public void setUser(String user) {
+		this.user = user;
+	}
 
-    public String getDireccion() {
-        return direccion;
-    }
+	public String getMail() {
+		return mail;
+	}
 
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
 
-    public String getTelefono() {
-        return telefono;
-    }
+	public String getDireccion() {
+		return direccion;
+	}
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
 
-    public String getTipo() {
-        return tipo;
-    }
+	public String getTelefono() {
+		return telefono;
+	}
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getTipo() {
+		return tipo;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 
-    @Override
-    public String toString() {
-        return "user [id=" + id + ", nombre=" + nombre + ", user=" + user + ", mail=" + mail + ", direccion="
-                + direccion + ", telefono=" + telefono + ", tipo=" + tipo + ", password=" + password + "]";
-    }
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+
+	public List<Orden> getOrdenes() {
+		return ordenes;
+	}
+
+	public void setOrdenes(List<Orden> ordenes) {
+		this.ordenes = ordenes;
+	}
+
+	@Override
+	public String toString() {
+		return "user [id=" + id + ", nombre=" + nombre + ", user=" + user + ", mail=" + mail + ", direccion="
+				+ direccion + ", telefono=" + telefono + ", tipo=" + tipo + ", password=" + password + "]";
+	}
 
 }
