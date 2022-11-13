27abf7df425e72a79c771e361b577994ccb9bd0f
egonzalezganum <egonzalezganum@gmail.com>
2022-11-13 00:26:09 -0300

    añadir productos al carrito y mostrarlos

--- src/main/java/com/demo/ecommerce/Controller/HomeController.java
+++ src/main/java/com/demo/ecommerce/Controller/HomeController.java
@@ -57,7 +57,7 @@ public class HomeController {
 	}
 
 	@PostMapping("/cart")
-	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
 		
 		DetalleOrden detalleOrden = new DetalleOrden();
 		Producto producto = new Producto();
@@ -66,6 +66,22 @@ public class HomeController {
 		Optional<Producto> optionalProducto = productoService.get(id);
 		log.info("producto añadido al carrito: {} ",optionalProducto.get());
 		log.info("cantidad: {} ",cantidad);
+		
+		producto = optionalProducto.get();
+		detalleOrden.setCantidad(cantidad);
+		detalleOrden.setPrecio(producto.getPrecio());
+		detalleOrden.setNombre(producto.getNombre());
+		detalleOrden.setTotal(producto.getPrecio()*cantidad);
+		detalleOrden.setProducto(producto);
+		 
+		detalles.add(detalleOrden);
+		
+		sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+		
+		orden.setTotal(sumaTotal);
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
 		
 		return "usuario/carrito";
 		
