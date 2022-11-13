d0f2b692e8a813e2e3c6e301da784872db1436bd
egonzalezganum <egonzalezganum@gmail.com>
2022-11-13 18:02:41 -0300

    metodo generar numeroOrden

--- src/main/java/com/demo/ecommerce/Service/IOrdenService.java
+++ src/main/java/com/demo/ecommerce/Service/IOrdenService.java
@@ -1,8 +1,13 @@
 package com.demo.ecommerce.Service;
 
+import java.util.List;
+
 import com.demo.ecommerce.model.Orden;
 
 public interface IOrdenService {
+
+	List<Orden> findAll();
+	
 	Orden save(Orden orden); 
 	
 }
--- src/main/java/com/demo/ecommerce/Service/OrdenServiceImp.java
+++ src/main/java/com/demo/ecommerce/Service/OrdenServiceImp.java
@@ -1,5 +1,8 @@
 package com.demo.ecommerce.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,43 @@ public class OrdenServiceImp implements IOrdenService {
 	@Override
 	public Orden save(Orden orden) {
 		return ordenRepository.save(orden);
+	}
+
+	@Override
+	public List<Orden> findAll() {
+		return ordenRepository.findAll();
+	}
+
+	@SuppressWarnings("null")
+	public String generarOrden() {
+
+		Integer num = 0;
+		String numConcat = "";
+
+		List<Orden> ordenes = findAll();
+
+		List<Integer> numeros = new ArrayList<Integer>();
+
+		ordenes.stream().forEach(orden -> numeros.add(Integer.parseInt(orden.getNumero())));
+
+		if (ordenes.isEmpty()) {
+			num = 1;
+		} else {
+
+			num = numeros.stream().max(Integer::compare).get();
+			num++;
+		}
+		if (num<10) {
+			numConcat = "000000000"+String.valueOf(num);
+		} else if(num<100) {
+			numConcat = "00000000"+String.valueOf(num);
+		}else if(num<1000) {
+			numConcat = "0000000"+String.valueOf(num);
+		}else if(num<10000) {
+			numConcat = "0000000"+String.valueOf(num);
+		}
+
+		return numConcat;
 	}
 
 }
